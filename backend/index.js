c7615a6f0acf081a264a3bd5348c8ad926f2245f
surajgsn07 <surajgn07@gmail.com>
2025-01-08 22:33:55 +0530

    urls changed

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin:"https://craftmyplate-ui.netlify.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
